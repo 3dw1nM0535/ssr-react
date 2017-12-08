806ebc7631b2d1c7401f7162e0c03245d24c5fc4
3dw1nM0535 <workockmoses@gmail.com>
2017-12-08 11:15:29 +0300

    initial commit

---
+++ src/shared/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './check-mark-1292787_960_720.png';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the React on the server</h2>
+        </div>
+        <p className="App-intro">Server side rendering bets on speed!</p>
+      </div>
+    );
+  }
+}
+
+export default App;

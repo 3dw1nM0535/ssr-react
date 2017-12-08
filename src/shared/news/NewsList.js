0039069c04f0e1af23c4b1b1d39658fa96bc597e
3dw1nM0535 <workockmoses@gmail.com>
2017-12-09 00:15:50 +0300

    news component dat handler

--- src/shared/news/NewsList.js
+++ src/shared/news/NewsList.js
@@ -1,2 +1,47 @@
 import React, { Component } from 'react';
-import from 
+import timeAgo from 'node-time-ago';
+import orderBy from 'lodash.orderby';
+import './NewsList.css';
+
+class NewsList extends Component {
+  render () {
+    return (
+      <div className="newslist">
+        <div className="header">
+          <div className="header-title">
+            <strong>Wizard News</strong>
+          </div>
+          <div className="sort">
+            Sort By: {' '}
+            <a
+              href='#'
+              className={this.state.sortOder === 'upvotes' && 'sort-selected'}
+              onClick={this.sortOrder.bind(this, 'upvotes')}>
+              Upvotes
+            </a>|
+            <a
+              href="#"
+              className={this.state.sortOrder === 'date' && 'sort-selected'}
+              onClick={this.sortOrder.bind(this, 'date')}>
+              Date
+            </a>
+          </div>
+        </div>
+
+        {news && news.map(post =>
+          <div key={post.id} className="news-item">
+            <p>
+              <span className='news-position'>{post.id}. </span> {post.title}{' '}
+                <small>(by {post.author})</small>
+            </p>
+            <small>
+              {post.upvotes} Upvotes | {timeAgo(post.date)}
+            </small>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default NewsList;
